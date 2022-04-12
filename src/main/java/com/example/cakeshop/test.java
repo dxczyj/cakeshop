3c6807b9d99fd71c0b4ef305724a5035d97438cc
董酱想当男模 <7816793+AuroraGod@user.noreply.gitee.com>
2022-04-12 15:07:40 +0800

    cakeshop 1.1

--- src/main/java/com/example/cakeshop/test.java
+++
@@ -1,9 +0,0 @@
-package com.example.cakeshop;
-
-import com.example.cakeshop.utils.DBUtil;
-
-public class test {
-    public static void main(String[] args) {
-        DBUtil.getDataSource();
-    }
-}
